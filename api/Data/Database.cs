a7e592cf943b9fcefab76689be17b72f284b3dcb
RoweDaBoat <rowegallagher@gmail.com>
2024-04-01 10:24:56 -0500

    1.2

--- api/Data/Database.cs
+++ api/Data/Database.cs
@@ -1,22 +1,17 @@
 using System.Reflection.Metadata.Ecma335;
-using MySql.Data.MySqlClient;
 
 namespace api.Data
 {
-    public class Database
+    public class Connectionstring
     {
-        private string server = "wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        private string port = "3306";
-        private string user = "g4c41xhtc7ijtkxd";
-        private string schema = "cjdic9hrkbhzny5e";
-        private string password = "qt1hsv20ohcyt3tv";
-
-        public MySqlConnection GetPublicConnection()
+        public ConnectionString()
         {
+            private string server = "wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+            private string port = "3306";
+            private string user = "g4c41xhtc7ijtkxd";
+            private string schema = "cjdic9hrkbhzny5e";
+            private string password = "qt1hsv20ohcyt3tv";
             string cs = $"server={server};user={user};database={schema};port={port};password={password}";
-            var con = new MySqlConnection(cs);
         }
-        
     }
-
 }
