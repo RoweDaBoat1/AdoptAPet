bce0222444fe481c1470142eb7f0ecc4bd127c6f
Maren O'Hara <maren_ohara@Marens-MacBook-Pro.local>
2024-04-08 10:59:45 -0500

    updatedviewpets

--- api/Program.cs
+++ api/Program.cs
@@ -5,6 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("OpenPolicy", 
+    builder => {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
