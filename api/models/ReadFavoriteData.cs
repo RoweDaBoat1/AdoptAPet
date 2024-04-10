f0eb95c8cc0311520ce53810531e754e4f947290
RoweDaBoat <rowegallagher@gmail.com>
2024-04-10 10:17:49 -0500

    favorites

---
+++ api/models/ReadFavoriteData.cs
@@ -0,0 +1,60 @@
+using api.Data;
+using api.models.interfaces;
+using MySql.Data.MySqlClient;
+
+namespace api.models
+{
+    public class ReadFavoriteData : IGetAllFavorites, IGetFavorite
+    {
+        public List<Favorites> GetAllFavorites()
+        {
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            string stm = "SELECT * FROM Favorites";
+            using var cmd = new MySqlCommand(stm, con);
+
+
+            using MySqlDataReader rdr = cmd.ExecuteReader();
+
+            List<Favorites> allFavorites = new List<Favorites>();
+            while(rdr.Read())
+            {
+                allFavorites.Add(new Favorites()
+                {
+                    PetID = rdr.IsDBNull(0) ? 0 : rdr.GetInt32(0),
+                    UserID = rdr.IsDBNull(1) ? 0 : rdr.GetInt32(1),
+                    FavoriteDate = rdr.IsDBNull(2) ? null : rdr.GetString(2)
+                });
+            }
+
+            return allFavorites;
+        }
+
+        public Favorites GetFavorite(int ID)
+        {
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            string stm = "SELECT * FROM Favorites WHERE PetID = @ID";
+            using var cmd = new MySqlCommand(stm, con);
+            cmd.Parameters.AddWithValue("@ID", ID);
+            cmd.Prepare();
+            using MySqlDataReader rdr = cmd.ExecuteReader();
+
+            rdr.Read();
+            return new Favorites()
+                {
+                    PetID = rdr.IsDBNull(0) ? 0 : rdr.GetInt32(0),
+                    UserID = rdr.IsDBNull(1) ? 0 : rdr.GetInt32(1),
+                    FavoriteDate = rdr.IsDBNull(2) ? null : rdr.GetString(2)
+                };
+        }
+    }
+}
